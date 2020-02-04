176d04311d2f867614bacf9218b5d4daf0e8dcad
konfiot <arthur.toussaint@polytechnique.edu>
2020-02-04 18:00:17 -0500

    Back to CSV

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from M2Crypto import SSL
 # For error handling
 import socket
 
-STORE = "MONGODB" #CSV, MONGO
+STORE = "CSV" #CSV, MONGO
 
 PURGE_DB = True # Reset DB on startup
 
@@ -111,11 +111,17 @@ def process(site):
 
 	if STORE == "CSV":
 		out_file = open(out_path, "a")
-		line = ",".join(out.values()) + "\n"
+		line = f"{out['site']},{out['http_version']},{out['answer_malformed']},{out['ssl_cipher']}\n"
 		out_file.write(line)
 		out_file.close()
+
 	elif STORE == "MONGODB":
 		mongo_db.sites.insert_one(out)
+
+if STORE == "CSV" and PURGE_DB:
+	out_file = open(out_path, "w")
+	out_file.write("site,http_version,answer_malformed,ssl_cipher\n")
+	out_file.close()
 
 
 with zipfile.ZipFile(zip_file) as zipopen:
